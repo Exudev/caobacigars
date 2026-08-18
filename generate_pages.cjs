e337c44dc2b46fb4f9c4c96020ebffd85cc586bc
Pablo Díaz <pablo.euardo10@gmail.com>
2026-08-18 14:04:03 -0400

    Add cache buster to favicon

--- generate_pages.cjs
+++ generate_pages.cjs
@@ -38,7 +38,7 @@ cigars.forEach(cigar => {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2">
   <title>${cigar.title} - Caoba Cigars</title>
   <link rel="stylesheet" href="/css/style.css">
 </head>
